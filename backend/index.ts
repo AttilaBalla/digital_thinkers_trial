44bff2e1c4d404c40f906ef69b293ad388981f5c
AttilaBalla <xattus@gmail.com>
2022-06-29 21:44:33 +0200

    rename folder, update gitignore

--- backend/index.ts
+++ backend/index.ts
@@ -1,14 +1,20 @@
 import express, { Express, Request, Response } from 'express';
 import dotenv from 'dotenv';
+import {Bootstrap} from "./bootstrap";
 
 dotenv.config();
 
 const app: Express = express();
 const port = process.env.PORT;
 
+//const driversRoute = require('/routes/formulaDriverRoutes.ts');
+// app.use('/drivers', driversRoute)
+
 app.get('/', (req: Request, res: Response) => {
     res.send('Express + TypeScript Server');
 });
+
+Bootstrap.bootstrapDriverData();
 
 app.listen(port, () => {
     console.log(`⚡️[server]: Server is running at http://localhost:${port}`);
