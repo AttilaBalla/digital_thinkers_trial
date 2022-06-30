900c20353ddacfe33f7897146bd4d4c4dc90a6e4
AttilaBalla <xattus@gmail.com>
2022-06-30 09:53:55 +0200

    add networking to frontend, update interfaces to match JSON keys

---
+++ frontend/utilities/networking.ts
@@ -0,0 +1,31 @@
+import {FormulaDriver} from "../types/FormulaDriver";
+
+function request<T>(url: string, method: string): Promise<T> {
+    const options = {
+        method,
+        headers: {
+            'Accept': 'application/json',
+            'Content-Type': 'application/json',
+        }
+    }
+
+    return fetch(`${process.env.NEXT_PUBLIC_API_BASE_URL}/api/${url}`, options)
+        .then((res) => {
+            if (res.ok) {
+                return res.json();
+            } else {
+                console.log(res);
+                return Promise.reject({
+                    status: res.status,
+                    type: res.type
+                });
+            }
+        })
+        .catch((error) => {
+            return Promise.reject(error);
+        });
+}
+
+export function getDrivers(): Promise<FormulaDriver[]> {
+    return request<FormulaDriver[]>('drivers', 'GET');
+}
