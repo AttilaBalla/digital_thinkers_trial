4937d0a242ea80fe5b97487bf874efc92d9303d3
AttilaBalla <xattus@gmail.com>
2022-06-30 11:26:58 +0200

    cleanup, add driver code to position label

--- frontend/components/PositionLabel.tsx
+++ frontend/components/PositionLabel.tsx
@@ -3,22 +3,30 @@ import {Typography} from "@mui/material";
 
 interface IProps {
     position: number
+    driverCode: string
 }
 
-export const PositionLabel:React.FC<IProps> = ({position}) => {
+export const PositionLabel: React.FC<IProps> = ({position, driverCode}) => {
 
     let suffix;
-    switch(position) {
-        case 1: suffix = 'st'
+    switch (position) {
+        case 1:
+            suffix = 'st'
             break
-        case 2: suffix = 'nd'
+        case 2:
+            suffix = 'nd'
             break
-        case 3: suffix = 'rd'
+        case 3:
+            suffix = 'rd'
             break
-        default: suffix = 'th'
+        default:
+            suffix = 'th'
     }
 
     return (
-        <Typography variant={'h5'} marginRight={'1rem'}>{`${position}${suffix}`}</Typography>
+        <React.Fragment>
+            <Typography variant={'h5'} marginRight={'.5rem'}>{`${position}${suffix}`}</Typography>
+            <Typography variant={'h5'} marginRight={'1rem'}>{driverCode}</Typography>
+        </React.Fragment>
     )
 }
